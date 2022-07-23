ebcbe742250153f5f1d189c6d482b85830f511b4
zhangxinqiang <1933581361@qq.com>
2022-07-24 00:17:37 +0800

    BorderLayoutDemo, BorderLayoutDemo2, GridLayoutDemo

--- swing-demo1/src/main/java/example/layout/BorderLayoutDemo2.java
+++ swing-demo1/src/main/java/example/layout/BorderLayoutDemo2.java
@@ -6,7 +6,7 @@ import java.awt.*;
  * @author : zxq
  * @create : 2022/7/23 10:33
  */
-public class BorderLayoutDemo {
+public class BorderLayoutDemo2 {
     public static void main(String[] args) {
         Frame frame = new Frame();
 
@@ -15,15 +15,12 @@ public class BorderLayoutDemo {
 
         frame.add(new Button("NORTH"), BorderLayout.NORTH);
         frame.add(new Button("SOUTH"), BorderLayout.SOUTH);
-        frame.add(new Button("EAST"), BorderLayout.EAST);
-        frame.add(new Button("WEST"), BorderLayout.WEST);
-        frame.add(new Button("CENTER"), BorderLayout.CENTER);
 
-        // 同一区域重复添加，会直接覆盖
-        frame.add(new Button("CENTER1"), BorderLayout.CENTER);
-        // 不指定区域，默认center
-        frame.add(new Button("CENTER2"));
+        Panel panel = new Panel();
+        panel.add(new Button("test button"));
+        panel.add(new TextField("test field"));
 
+        frame.add(panel);
 
         // pack 设置最佳大小
         frame.pack();
---
+++ swing-demo1/src/main/java/example/layout/GridLayoutDemo.java
@@ -0,0 +1,37 @@
+package example.layout;
+
+import java.awt.*;
+
+/**
+ * @author : zxq
+ * @create : 2022/7/23 10:33
+ */
+public class GridLayoutDemo {
+    public static void main(String[] args) {
+        Frame frame = new Frame("calc");
+
+        // pane1
+        // textFiled
+        Panel panel = new Panel();
+        panel.add(new TextField(30));
+        frame.add(panel, BorderLayout.NORTH);
+
+        // panel2
+        // 数字1~9
+        Panel panel2 = new Panel(new GridLayout(3, 5, 4, 4));
+        for (int i = 0; i < 10; i++) {
+            panel2.add(new Button(String.valueOf(i)));
+        }
+        // 加减乘除
+        String[] ff = {"+", "-", "*", "/", "."};
+        for (String f : ff) {
+            panel2.add(new Button(f));
+        }
+        frame.add(panel2);
+
+
+        // pack 设置最佳大小
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
